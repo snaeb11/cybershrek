c95ea0e86faeb1b1367646f47e586f2e3f589c14
Lanz MGTan <lanzmgtan@gmail.com>
2025-01-05 12:28:43 +0800

    fixed logs to view latest (desc - time, date)

--- fetch_logs.php
+++ fetch_logs.php
@@ -5,7 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$sql = "SELECT email, activity_log, date, time FROM logs"; // Replace 'logs' with your actual table name
+// Query to select logs ordered by date and time in descending order
+$sql = "SELECT email, activity_log, date, time FROM logs ORDER BY date DESC, time DESC";
+
 $result = $conn->query($sql);
 
 $data = [];
@@ -14,6 +16,10 @@ if ($result->num_rows > 0) {
         $data[] = $row;
     }
 }
+
+// Return the data as a JSON response
+header('Content-Type: application/json');
 echo json_encode($data);
+
 $conn->close();
 ?>
